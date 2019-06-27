83e922bbaf7acc609c9a7f20b4ad1c7be228aae9
flexa16 <kasabutskiy.vlad@gmail.com>
2019-06-27 20:04:39 +0300

    dd

---
+++ server/src/main/java/by/pvt/Server.java
@@ -0,0 +1,16 @@
+package by.pvt;
+
+import java.net.ServerSocket;
+
+public class Server {
+
+
+    public static void main(String[] args) {
+
+        ServerSocket serverSocket = new ServerSocket()
+
+
+    }
+
+
+}
